2195f5288b2421a57c32b27aec1b75d20448de37
Xulixiao Tian <allietianx@gmail.com>
2018-05-28 20:24:19 -0700

    Add timestamp and cool down for dishes.

--- src/DishRandomizer.java
+++ src/DishRandomizer.java
@@ -6,28 +6,65 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class DishRandomizer {
-    public class Dish {
+    // Dish cool down currently set to 7 days.
+    private static final long COOL_DOWN = 604800000;
+
+    private static final String FILE_PATH = "./data/Dishes.csv";
+
+    public static class Dish {
         String name;
-        long lastMade;
-        long position;
-        public Dish (String name, long lastMade, long position) {
+        long lastTimeStamp;
+        int position;
+
+        public Dish(String name, long lastTimeStamp, int position) {
             this.name = name;
-            this.lastMade = lastMade;
+            this.lastTimeStamp = lastTimeStamp;
             this.position = position;
+        }
+
+        public static List<Dish> fromLines(List<String> lines) {
+            List<Dish> dishes = new ArrayList<>();
+            for (int i = 0; i < lines.size(); i++) {
+                String[] line = lines.get(i).split(",");
+                dishes.add(new Dish(line[0], Long.valueOf(line[1]), i));
+            }
+            return dishes;
         }
     }
 
     public static void main (String[] args) {
-        List<String> dishes = readLines("./data/Dishes.csv");
+        List<String> lines = readLines(FILE_PATH);
+        List<Dish> dishes = Dish.fromLines(lines).stream()
+                .filter(a->System.currentTimeMillis() - a.lastTimeStamp > COOL_DOWN).collect(Collectors.toList());
+
+        if(dishes.isEmpty()) {
+            System.out.println("We run out of dishes :(");
+            return;
+        }
 
         Random rand = new Random();
-        int value = rand.nextInt(dishes.size());
+        Dish dish = dishes.get(rand.nextInt(dishes.size()));
 
-        System.out.println(String.format("Today we're going to have %s !", dishes.get(value)));
+        System.out.println(String.format("Today we're going to have %s!", dish.name));
+
+        lines.set(dish.position, dish.name+","+System.currentTimeMillis());
+
+        updateFile(FILE_PATH, lines);
     }
 
+    private static void updateFile(String fileName, List<String> lines) {
+        Path path = Paths.get(fileName);
+        Charset charset = Charset.forName("UTF-8");
+
+        try {
+            Files.write(path, lines, charset);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     private static List<String> readLines (String fileName) {
         Path path = Paths.get(fileName);
